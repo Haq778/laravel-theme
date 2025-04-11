31ddfb0bd159a4c63b19f2a074330fce17326bdf
Haq778 <ahmadbaihaqi.july@gmail.com>
2025-04-11 09:53:25 +0700

    laravel-theme

---
+++ laravel-theme/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ThemeController;
+
+Route::get('/', [ThemeController::class, 'index']);
+Route::get('/theme/{theme}', [ThemeController::class, 'setTheme']);
+
+
